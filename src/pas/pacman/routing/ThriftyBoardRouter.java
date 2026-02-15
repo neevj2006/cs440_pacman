7a344837eb582363fbf2277c1610169876b5c664
Neev Jain <neev0511jain@gmail.com>
2026-02-15 18:12:08 -0500

    Implemented ThirftyBoardRouter with Djiktras

--- src/pas/pacman/routing/ThriftyBoardRouter.java
+++ src/pas/pacman/routing/ThriftyBoardRouter.java
@@ -1,9 +1,7 @@
 package src.pas.pacman.routing;
 
-
 // SYSTEM IMPORTS
 import java.util.Collection;
-
 
 // JAVA PROJECT IMPORTS
 import edu.bu.pas.pacman.agents.Agent;
@@ -16,48 +14,40 @@ import edu.bu.pas.pacman.routing.BoardRouter.ExtraParams;
 import edu.bu.pas.pacman.utils.Coordinate;
 import edu.bu.pas.pacman.utils.Pair;
 
-import java.util.ArrayList;
-import java.util.HashSet;       // will need for bfs
-import java.util.Queue;         // will need for bfs
-import java.util.LinkedList;    // will need for bfs
-import java.util.Set;
-
 // This class is responsible for calculating routes between two Coordinates on the Map.
 // Use this in your PacmanAgent to calculate routes that (if followed) will lead
 // Pacman from some Coordinate to some other Coordinate on the map.
 public class ThriftyBoardRouter
-    extends BoardRouter
-{
+        extends BoardRouter {
 
-    // If you want to encode other information you think is useful for Coordinate routing
+    // If you want to encode other information you think is useful for Coordinate
+    // routing
     // besides Coordinates and data available in GameView you can do so here.
     public static class BoardExtraParams
-        extends ExtraParams
-    {
+            extends ExtraParams {
 
     }
 
     // feel free to add other fields here!
 
     public ThriftyBoardRouter(int myUnitId,
-                              int pacmanId,
-                              int ghostChaseRadius)
-    {
+            int pacmanId,
+            int ghostChaseRadius) {
         super(myUnitId, pacmanId, ghostChaseRadius);
 
         // if you add fields don't forget to initialize them here!
     }
 
-
     @Override
     public Collection<Coordinate> getOutgoingNeighbors(final Coordinate src,
-                                                       final GameView game,
-                                                       final ExtraParams params)
-    {
-        final Collection<Coordinate> neighbors = new ArrayList<>(4);
-        for (final Action a : Action.values()) {
-            if (game.isLegalPacmanMove(src,a)) {
-                neighbors.add(a.apply(src));
+            final GameView game,
+            final ExtraParams params) {
+
+        Collection<Coordinate> neighbors = new java.util.ArrayList<>(4);
+
+        for (Action action : Action.values()) {
+            if (game.isLegalPacmanMove(src, action)) {
+                neighbors.add(action.apply(src));
             }
         }
         return neighbors;
@@ -65,30 +55,39 @@ public class ThriftyBoardRouter
 
     @Override
     public Path<Coordinate> graphSearch(final Coordinate src,
-                                        final Coordinate tgt,
-                                        final GameView game)
-    {
-        if (src.equals(tgt)) {
-            return new Path<>(src);
-        }
+            final Coordinate tgt,
+            final GameView game) {
 
-        final Queue<Path<Coordinate>> q = new LinkedList<>();
-        final Set<Coordinate> visited = new HashSet<>();
+        java.util.PriorityQueue<Path<Coordinate>> frontier = new java.util.PriorityQueue<>(
+                java.util.Comparator.comparing(Path::getTrueCost));
 
-        q.add(new Path<>(src));
-        visited.add(src);
+        java.util.Set<Coordinate> visited = new java.util.HashSet<>();
 
-        while (!q.isEmpty()) {
-            final Path<Coordinate> curPath = q.remove();
-            final Coordinate cur = curPath.getDestination();
+        Path<Coordinate> startPath = new Path<>(src);
 
-            if (cur.equals(tgt)) {
-                return curPath;
+        frontier.add(startPath);
+
+        while (!frontier.isEmpty()) {
+
+            Path<Coordinate> currentPath = frontier.poll();
+            Coordinate current = currentPath.getDestination();
+
+            if (visited.contains(current)) {
+                continue;
             }
 
-            for (final Coordinate nbr : getOutgoingNeighbors(cur, game, new BoardExtraParams())) {
-                if (visited.add(nbr)) {
-                    q.add(new Path<>(nbr, 1.0f, curPath));
+            visited.add(current);
+
+            if (current.equals(tgt)) {
+                return currentPath;
+            }
+
+            for (Coordinate neighbor : getOutgoingNeighbors(current, game, null)) {
+
+                if (!visited.contains(neighbor)) {
+                    Path<Coordinate> newPath = new Path<>(neighbor, 1.0f, currentPath);
+
+                    frontier.add(newPath);
                 }
             }
         }
@@ -96,4 +95,3 @@ public class ThriftyBoardRouter
     }
 
 }
-
